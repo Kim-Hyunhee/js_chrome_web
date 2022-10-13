d138b9e8b1f327d9cc2bdec55622cc1e02aa08eb
Kim-Hyunhee <96518301+Kim-Hyunhee@users.noreply.github.com>
2022-10-13 15:56:37 +0900

    eventListener

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+console.dir(title);
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
