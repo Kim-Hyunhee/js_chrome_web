ab0722296b784bc1a7315a842fb19bc47897afae
Kim-Hyunhee <96518301+Kim-Hyunhee@users.noreply.github.com>
2022-10-25 13:56:02 +0900

    saving Username

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(info) {
   info.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem = ("username", username);
   greeting.innerText = `Hello ${username} keep going`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
