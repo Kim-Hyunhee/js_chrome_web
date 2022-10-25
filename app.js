869ebbd19ff3380ab4b9a9340cf2bbf8ec0740af
Kim-Hyunhee <96518301+Kim-Hyunhee@users.noreply.github.com>
2022-10-25 11:44:46 +0900

    getting username

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(info) {
   info.preventDefault();
-  console.log(info);
-}
-
-function handleLinkClick(click) {
-  click.preventDefault();
-  alert("click");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username} keep going`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
