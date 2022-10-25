0489e74a6efafe5049a2091dfc24916d7bc05201
Kim-Hyunhee <96518301+Kim-Hyunhee@users.noreply.github.com>
2022-10-25 11:13:53 +0900

    events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(info) {
+  info.preventDefault();
+  console.log(info);
 }
 
-loginButton.addEventListener("click", handleBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
