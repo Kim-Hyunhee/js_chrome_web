d55c6d071302f64a2938f6a3fdd1d98eefbe7508
Kim-Hyunhee <96518301+Kim-Hyunhee@users.noreply.github.com>
2022-10-25 11:35:26 +0900

    evnetx part two

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(info) {
   info.preventDefault();
   console.log(info);
 }
 
+function handleLinkClick(click) {
+  click.preventDefault();
+  alert("click");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
